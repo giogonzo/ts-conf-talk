7e3fbd924d7c9fb58903e223610c22f9283a2084
Giovanni Gonzaga <giovanni@buildo.io>
2019-10-19 19:33:16 +0200

    enforce all endpoints are implemented server-side

--- api/express.ts
+++ api/express.ts
@@ -2,10 +2,33 @@ import { Application, json } from "express";
 import { Endpoint } from "../shared/dsl";
 import { isLeft } from "fp-ts/lib/Either";
 import { failure } from "io-ts/lib/PathReporter";
+import * as t from "io-ts";
+import { record } from "fp-ts/lib/Record";
+
+/**
+ * Implement the defined `api` with `implementations`, and expose the routes as POST Express handlers
+ * @param app An express() application
+ * @param api The API definition
+ * @param implementations A bag of API implementations - must match the definition
+ */
+export function addAPIToExpress<A extends Record<string, Endpoint<any, any>>>(
+  app: Application,
+  api: A,
+  implementations: {
+    [K in keyof A]: Controller<
+      t.TypeOf<A[K]["input"]>,
+      t.TypeOf<A[K]["output"]>
+    >;
+  }
+): void {
+  record.mapWithIndex(api, (k, endpoint) => {
+    addEndpointToExpress(app, endpoint, implementations[k]);
+  });
+}
 
 type Controller<I, O> = (input: I) => Promise<O>;
 
-export function addEndpointToExpress<I, O>(
+function addEndpointToExpress<I, O>(
   app: Application,
   endpoint: Endpoint<I, O>,
   controller: Controller<I, O>
--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,13 @@
 import * as express from "express";
 import * as cors from "cors";
 import * as controllers from "./controllers";
-import { addEndpointToExpress } from "./express";
+import { addAPIToExpress } from "./express";
 import { api } from "../shared/api";
 
 const app = express();
 
 app.use(cors());
 
-addEndpointToExpress(app, api.createUser, controllers.createUser);
-addEndpointToExpress(app, api.listUsers, controllers.listUsers);
+addAPIToExpress(app, api, controllers);
 
 app.listen(3456);
